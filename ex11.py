428088b0b5dc4f4a9ff972f509b30372c726399c
Adam Ingrassia <adampsyche@gmail.com>
2012-02-20 16:51:22 +0100

    Fix print statement with correct string substitution.

--- ex11.py
+++ ex11.py
@@ -6,4 +6,4 @@ height = raw_input()
 print "How much do you weigh?",
 weight = raw_input()
 
-print "So, you're %r old, %r tall, and %r heavy." (age, height, weight)
+print "So, you're %r old, %r tall, and %r heavy." % (age, height, weight)
