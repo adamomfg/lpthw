1ff555e8ac985f0b1f4a6b70208f5d29c7a325f0
Adam Ingrassia <adampsyche@gmail.com>
2012-02-21 14:22:48 +0100

    A few exercises (and permissions changes.)

---
+++ ex11ec1.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+print "The docs say \"If the prompt argument is present, it is written to standard output without a trailing newline.\"\n"
+
+print "This function converts what you type to a string, and returns that."
