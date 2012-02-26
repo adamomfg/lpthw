8178e2047f01d94ac879e89db2c64407427184b1
Adam Ingrassia <adampsyche@gmail.com>
2012-02-26 12:59:55 -0500

    Exercise 13.

---
+++ ex13.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "The first variable is called:", first
+print "The second variable is called:", second 
+print "The third variable is called:", third 
