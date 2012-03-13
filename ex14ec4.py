9d1832540ff4a4373596e4c890f8d105c62cd355
Adam Ingrassia <adampsyche@gmail.com>
2012-03-13 16:03:59 -0700

    Explain that I understand the """ style multi-line and % format activator.

---
+++ ex14ec4.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+print """
+The \"\"\" multi-line string was combined with the % format activator 
+for the last print. This was done invoking the raw format of the object in
+the given cases, and I introduced the string formatter for something that
+I knew was a string in the other case.
+"""
