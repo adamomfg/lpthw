b011323b907190db5702f61787fa395fc3ec7d3a
Adam Ingrassia <adampsyche@gmail.com>
2012-03-13 15:42:46 -0700

    I researched Zork a bit; will play around with it after perf.

---
+++ ex14ec1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+print """
+Zork distinguished itself in its genre as an especially rich game, in terms of both the quality of the storytelling and the sophistication of its text parser, which was not limited to simple verb-noun commands ("hit troll"), but recognized some prepositions and conjunctions ("hit the troll with the Elvish sword").\n
+"""
+print "The author of the exercise clearly wants to drive home the point that the commands are where the action is."
