058e06a96b9e7f07d5fb894e4747ddf7de538342
Adam Ingrassia <adampsyche@gmail.com>
2012-03-17 15:44:36 -0400

    Reading files.

---
+++ ex15ec3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+print """
+I noticed the use of the word command, and found it somewhat odd. But, it makes
+more sense the more I thought about it. A command is telling something to do something, and maybe in some way (that'd be a parameter, and there may be a default.) Functions and methods are the only variation that I've seen, or at least noticed. Routine? Perhaps.
+"""
