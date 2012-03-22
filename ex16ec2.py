793ac7e8024244845eaa1427ddb246e012a68ff7
Adam Ingrassia <adampsyche@gmail.com>
2012-03-21 21:55:47 -0400

    Use read and argv to read the file just created

--- ex16ec2.py
+++ ex16ec2.py
@@ -5,13 +5,11 @@ from sys import argv
 script, filename = argv
 
 txt = open(filename)
-
-print "Here's your file %r:" % filename
 print txt.read()
 
-print "Type the filename again:"
-file_again = raw_input("")
+print "Here's your file %r:" % filename
+
+file_again = raw_input("Type the filename again: ")
 
 txt_again = open(file_again)
-
 print txt_again.read()
