1d09aae79af613f427feac21bd79adb0fe3f5552
Adam Ingrassia <adampsyche@gmail.com>
2012-03-28 08:27:29 -0400

    Reading and writing files.

---
+++ ex16ec4.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python
+print "open() defaults to 'r' mode if mode is omitted. so you don't overwrite your stuff on accident."
---
+++ ex17.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+from sys import argv
+from os.path import exists
+
+scropt, from_file, to_file = argv
+
+# we coudl do these two on one lin too, how?
+input = open(from_file)
+indata = input.read()
+
+print )
