bfe7852a88e9b38c7294005910cf164ad84a9299
Adam Ingrassia <adampsyche@gmail.com>
2012-03-31 16:48:15 -0400

    Start of file that will return values from functions.

---
+++ ex21.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+def add(a, b):
+  print "ADDING %d + %d" % (a, b)
+  return a + b
+  
+def subtract(a, b):
+  print "SUBTRACTING %d - %d" % (a, b)
+  
+def multiply(a, b):
+  print "MULTIPLYING %d * %d" % (a, b)
+  
+def divide(a, b):
+  print "DIVIDING %d / %d" % (a, b)
+  
+print "Let's do some math with just functions!"
