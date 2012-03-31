f88a12824cefa969f8b36214ce5d4c6714c0c74c
Adam Ingrassia <adampsyche@gmail.com>
2012-03-31 16:51:09 -0400

    Finishing file with functions that return values.

--- ex21.py
+++ ex21.py
@@ -13,3 +13,17 @@ def divide(a, b):
   print "DIVIDING %d / %d" % (a, b)
   
 print "Let's do some math with just functions!"
+
+age = add(30, 5)
+height =  subtract(78, 4)
+weight = multiply (90, 2)
+iq = divide(100, 2)
+
+print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, ip)
+
+# A puzzle for the extra credit, type it anyway
+print "Here is a puzzle."
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print "That becomes: ", what, "Can you do it by hand?"
