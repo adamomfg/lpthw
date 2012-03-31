e6a978a34923261bf86f1d6f145facbe376f3136
Adam Ingrassia <adampsyche@gmail.com>
2012-03-31 16:44:37 -0400

    Explain what seek does.

---
+++ ex20ec4.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+print "The seek method takes an offset argument by which to advance the file's "
+"current position. The default value for this is 0, for absolute file positioning."
