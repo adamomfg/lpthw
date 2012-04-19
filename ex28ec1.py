a7ac8d3213398a7f94285f709255f03766bbf0c2
Adam Ingrassia <adampsyche@gmail.com>
2012-04-18 21:52:06 -0400

    More equality operators.

---
+++ ex28ec1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+print 5 > 1
+print 5 >= 1
+print 5 <= 5
+print 5 != 0
+print 5 is not 1
+print 5 is 5
+print 0 is not False
+print '' is not False
+print '' is None
+print [] is False
+print None is False
+print {} is False
+print '' is not True
